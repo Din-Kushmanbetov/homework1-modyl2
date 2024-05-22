65a1aa3f5ef5c9974248f37ea2b11e0c17b7d057
Din-Kushmanbetov <aiddenpes@gmail.com>
2024-05-22 20:13:55 +0300

    Add files via upload

---
+++ homework1_block2.py
@@ -0,0 +1,22 @@
+x = 38
+print ('vallhalla')
+if x < 0:
+    print ('меньше нуля')
+print ('hell')
+a, b = 123, 231
+if a < b:
+    print ('win')
+if a < b and a > 0:
+    print ("wictory")
+if a < b and (a > 0 or b > 200):
+    print ("very nice")
+if 100 < b and b < 10239:
+    print ('ow la la')
+if 11 < 22:
+    print ('win')
+if [2,2,3,5,2] > [1,1,2,3,1]:
+    print ('winery')
+#if '6' > 4:
+#    print('lose')
+if '12' != 2:
+    print ('what is?')
